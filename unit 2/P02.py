fa3258bd7aaee5caa8e65f647e6eb5e9cca9b04c
shivamgupta152136 <shivam.gupta152136@marwadiuniversity.ac.in>
2026-08-31 22:16:57 +0530

    p02

---
+++ unit 2/P02.py
@@ -0,0 +1,10 @@
+
+number = float(input("Enter a number: "))
+
+if number >= 0:
+    if number == 0:
+        print("The number is zero.")
+    else:
+        print("The number is positive.")
+else:
+    print("The number is negative.")
